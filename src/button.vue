035a7489e350290fb9c1a06c2ae14fb7a1c51985
“hj207255” <2540991545@qq.com>
2019-08-18 11:18:30 +0800

    给按钮添加icon，点击更换icon，且添加按钮组

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,36 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button @click="$emit('click')" class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <g-icon v-if="icon && !loading" :name="icon"></g-icon>
+    <g-icon v-if="loading" class="loading icon" name="jiazai"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default{}
+  export default{
+    // props: ['icon','iconPosition']
+    props: {
+      "icon": {},
+      "loading": {
+        type: Boolean,
+        default: false
+      },
+      "iconPosition": {
+        type: String,
+        default: 'left',
+        validator: function(value){
+          return !(value!=='left'&&value!=='right')
+        }
+      }
+    }
+  }
 </script>
 <style lang="scss">
+  @keyframes xuanzhuan {
+    0%{transform: rotate(0deg);}
+    100%{transform: rotate(360deg);}
+  }
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -12,7 +38,10 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
-
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover {
     border-color: var(--border-color-hover);
     }
@@ -21,6 +50,17 @@
     }
     &:focus {
       outline: none;
+    }
+    &.icon-right {
+      > .g-icon {order: 2; margin-left: .3em;}
+      > .content {order: 1;}
+    }
+    &.icon-left {
+      > .g-icon {order: 1; margin-right: .3em;}
+      > .content {order: 2;}
+    }
+    & .loading {
+      animation: xuanzhuan 3s infinite linear;
     }
   }
 </style>
